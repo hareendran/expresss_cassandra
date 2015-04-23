ebd88da28389e388ce82d567236ec7bf828f6861
hachandr <hachandr@akamai.com>
2015-04-23 18:05:19 -0400

    yayyyyyy the client is working

--- public/client.js
+++ public/client.js
@@ -6,8 +6,54 @@ search.addEventListener('keyup', function(){
   xhr.open('GET', '/users/search/' + search.value, true);
   xhr.onreadystatechange = function(){
     if (4 == xhr.readyState) {
-      code.textContent = xhr.responseText;
+      var ajaxResponse = xhr.responseText;
+      //code.textContent = xhr.responseText;
+      createTable(ajaxResponse);
+
+
     }
   };
   xhr.send();
 }, false);
+
+
+function  createTable(data){
+
+
+var fragment = document.createDocumentFragment();
+var response = JSON.parse(data);
+console.log('resp',response);
+document.getElementById("here_table").innerHTML = '';
+for (i = 0; i < response.rows.length; i++) {
+    var tr = document.createElement("tr");
+//[{"home_id":"H00999943","datetime":"2014-05-23T22:56:23.000Z",
+//"code_used":"1245","event":"alarm turned off"}
+
+    var singleRow = response.rows[i];
+
+    var td2 = document.createElement("td");
+    td2.innerHTML = singleRow.home_id;
+    tr.appendChild(td2);
+
+    var td3 = document.createElement("td");
+    td3.innerHTML = singleRow.datetime;
+    tr.appendChild(td3);
+
+    var td4 = document.createElement("td");
+    td4.innerHTML = singleRow.code_used;
+    tr.appendChild(td4);
+
+    var td5 = document.createElement("td");
+    td5.innerHTML = singleRow.event;
+    tr.appendChild(td5);
+    //does not trigger reflow
+    fragment.appendChild(tr);
+}
+
+var table = document.createElement("table");
+table.border = 1;
+
+table.appendChild(fragment);
+
+document.getElementById("here_table").appendChild(table);
+}
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,19 @@ var cassandra = require('cassandra-driver');
 var async = require('async');
 var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'home_security'});
 
-
+var getHomeById = 'SELECT * FROM activity where home_id=? ';
+client.connect(function(err, result) {
+    console.log('Connected.');
+});
 router.get('/search/:query?', function(req, res){
   var query = req.params.query;
   console.log("query is",query);
+  client.execute(getHomeById, [query ], function(err, result) {
+       if (err) {
+           res.status(404).send({ msg : 'Home not found.'+err });
+       } else {
+           res.json(result);        }
+   });
 });
 
 module.exports = router;
